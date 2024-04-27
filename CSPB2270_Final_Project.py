c7471ef652ee6a7cc02e9bcc9ab49222868280e2
Andrew Schiavetta <drew.schiavetta@gmail.com>
2024-04-27 21:51:21 +0000

    did this work

--- CSPB2270_Final_Project.py
+++ CSPB2270_Final_Project.py
@@ -1,1 +1,25 @@
+import numpy as np
+import math
+import random
+
+
+Row = int(input("Enter the number of rows:"))
+Column = int(input("Enter the number of columns:"))
+ 
+# Initialize matrix
+matrix = []
+
+# A for loop for row entries
+for row in range(Row):    
+    a = []
+    # A for loop for column entries
+    for column in range(Column):   
+        a.append(random.randint(1, 10))
+    matrix.append(a)
+ 
+# For printing the matrix
+for row in range(Row):
+    for column in range(Column):
+        print(matrix[row][column], end=" ")
+    print()
 
