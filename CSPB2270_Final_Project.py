e3a45e845386fa5be06d2f1ab1eaba268d6218bc
Andrew Schiavetta <drew.schiavetta@gmail.com>
2024-04-30 06:47:29 +0000

    strassen algorithm added

--- CSPB2270_Final_Project.py
+++ CSPB2270_Final_Project.py
@@ -3,39 +3,39 @@ import math
 import random
 
 
-Row = int(input("Enter the number of rows:"))
-Column = int(input("Enter the number of columns:"))
+row = int(input("Enter the number of rows:"))
+column = int(input("Enter the number of columns:"))
  
 # create matrix1
 matrix1 = []
 print("matrix1:", end ="\n")
-for row in range(Row):    
+for r in range(row):    
     temp = []
     # A for loop for column entries
-    for column in range(Column):   
+    for c in range(column):   
         temp.append(random.randint(1, 3))
     matrix1.append(temp)
  
 # print matrix1
-for row in range(Row):
-    for column in range(Column):
-        print(matrix1[row][column], end=" ")
+for r in range(row):
+    for c in range(column):
+        print(matrix1[r][c], end=" ")
     print()
 
 #create matrix2
 matrix2 = []
-for row in range(Row):    
+for r in range(row):    
     temp = []
     # A for loop for column entries
-    for column in range(Column):   
+    for c in range(column):   
         temp.append(random.randint(1, 3))
     matrix2.append(temp)
  
 #print matrix2
 print("matrix2:", end ="\n")
-for row in range(Row):
-    for column in range(Column):
-        print(matrix2[row][column], end=" ")
+for r in range(row):
+    for c in range(column):
+        print(matrix2[r][c], end=" ")
     print()
 
 
@@ -53,13 +53,71 @@ for i in range(len(matrix1)):
 
 #print Algorithm1 result matrix
 print("result matrix:", end ="\n")
-for row in range(Row):
-    for column in range(Column):
-        print(result_matrix1[row][column], end=" ")
+for r in range(row):
+    for c in range(column):
+        print(result_matrix1[r][c], end=" ")
     print()
 
 
 #Algorithm 2
+# def split(matrix):
+#     x = np.array(matrix1)
+#     row1, col1 = x.shape
+#     row2, col2 = row1//2, col1//2
+#     return x[:row2, :col2], x[:row2, col2:], x[row2:, :col2], x[row2:, col2:]
 
+
+
+
+def strassen(matrix1, matrix2):
+
+    x = np.array(matrix1) 
+    y = np.array(matrix2)
+
+    if x.size == 1 or y.size == 1:
+        return x * y
+
+    n = x.shape[0]
+
+    if n % 2 == 1:
+        x = np.pad(x, (0, 1), mode='constant')
+        y = np.pad(y, (0, 1), mode='constant')
+
+    m = int(np.ceil(n / 2))
+
+    # Split matrix 1
+    a = x[: m, : m]
+    b = x[: m, m:]
+    c = x[m:, : m]
+    d = x[m:, m:]
+
+    # Split matrix 2
+    e = y[: m, : m]
+    f = y[: m, m:]
+    g = y[m:, : m]
+    h = y[m:, m:]    
+
+    
+
+    p1 = strassen(a, f - h)  
+    p2 = strassen(a + b, h)        
+    p3 = strassen(c + d, e)        
+    p4 = strassen(d, g - e)        
+    p5 = strassen(a + d, e + h)        
+    p6 = strassen(b - d, g + h)  
+    p7 = strassen(a - c, e + f)  
+
+    c11 = p5 + p4 - p2 + p6  
+    c12 = p1 + p2           
+    c21 = p3 + p4            
+    c22 = p1 + p5 - p3 - p7
+
+    result = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+
+    return result
+
+
+result_matrix2 = strassen(matrix1, matrix2)
+print(result_matrix2)
 
 
